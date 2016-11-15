f979c92e57e5ac9e9f8a5aba7f09738db0f65338
Alexander Morozov <ntcomp12@gmail.com>
2016-11-15 22:16:43 +0300

    remade tests for iplirconf and nodename

--- spec/vipnet_parser/iplirconf_spec.rb
+++ spec/vipnet_parser/iplirconf_spec.rb
@@ -2,9 +2,12 @@ require "spec_helper"
 
 describe VipnetParser do
   require "yaml"
-  iplirconf = file_fixture("iplirconf/iplir.conf")
-  yaml = yaml_fixture("iplirconf/iplirconf.yml")
+  iplirconf_file = file_fixture("iplirconf/iplir.conf")
+  expected_iplirconf_hash = yaml_fixture("iplirconf/iplirconf.yml")
 
   it "should parse iplirconf" do
+    actual_iplirconf = VipnetParser::Iplirconf.new(iplirconf_file)
+    actual_iplirconf.parse(:hash)
+    expect(actual_iplirconf.hash).to eq(expected_iplirconf_hash)
   end
 end
--- spec/vipnet_parser/nodename_spec.rb
+++ spec/vipnet_parser/nodename_spec.rb
@@ -1,9 +1,12 @@
 require "spec_helper"
 
 describe VipnetParser do
-  content = file_fixture("nodename/nodename.doc")
-  yaml = yaml_fixture("nodename/nodename.yml")
+  nodename_file = file_fixture("nodename/nodename.doc")
+  expected_nodename_hash = yaml_fixture("nodename/nodename.yml")
 
   it "should parse nodename" do
+    actual_nodename = VipnetParser::Nodename.new(nodename_file)
+    actual_nodename.parse(:hash)
+    expect(actual_nodename.hash).to eq(expected_nodename_hash)
   end
 end
