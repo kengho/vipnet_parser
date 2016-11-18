e8085b70611d1b01a92d0a6f1599aeeaa6a7dcb5
Alexander Morozov <ntcomp12@gmail.com>
2016-11-19 01:20:17 +0300

    removed unneeded function

--- lib/vipnet_parser/vipnet_config.rb
+++ lib/vipnet_parser/vipnet_config.rb
@@ -1,11 +1,4 @@
 module VipnetParser
   class VipnetConfig
-    def ==(other)
-      res = true
-      @props.each do |prop|
-        res = res && self.send(prop.to_s) == other.send(prop.to_s)
-      end
-      res
-    end
   end
 end
