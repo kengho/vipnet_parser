0a2a0f7b8e7a856c984539b535c0927a7afc14ef
Alexander Morozov <ntcomp12@gmail.com>
2016-11-19 18:25:56 +0300

    removed unnecessary require in iplirconf_spec

--- spec/vipnet_parser/iplirconf_spec.rb
+++ spec/vipnet_parser/iplirconf_spec.rb
@@ -1,7 +1,6 @@
 require "spec_helper"
 
 describe VipnetParser do
-  require "yaml"
   iplirconf_file = file_fixture("iplirconf/iplir.conf")
   expected_iplirconf_hash = yaml_fixture("iplirconf/iplirconf.yml")
 
